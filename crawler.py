fb35734f46e18df59d8ba4263edf64b124b61dad
wendi123 <wendi123@bitbucket.org>
2015-10-12 14:16:14 +0700

    fixbug url image not start with absolute path

--- crawler.py
+++ crawler.py
@@ -11,9 +11,19 @@ def getCrawler(url):
         print 'Jumlah Image : %s' % len(soup.find_all('img'))
 
         for imgnya in soup.find_all('img'):
-            file_size_img = urllib.urlopen(imgnya.get('src')).info()['Content-Length']
+            src = imgnya.get('src')
+            if src.startswith('//'):
+                file_img = 'http:%s' % src
+            elif src.startswith('http://') or src.startswith('https://'):
+                file_img = src
+            else:
+                file_img = url +'/'+ src
+
+            print file_img
+            file_size_img = urllib.urlopen(file_img).info()['Content-Length']
             print 'Size image %s = %s bytes' % (imgnya.get('src'), file_size_img)
     else:
-        print 'Web can not Accessible. Status : %s' % r.status_code
+        print 'Can\'t Access Website. Status : %s' % r.status_code
 
 getCrawler("http://detik.com")
+print 'Done'
